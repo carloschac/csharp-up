3c6878b6313d99c1ff6a7482a13417ec37bedee7
CarinaRosa <carloss.chac@gmail.com>
2022-08-11 21:42:17 -0300

    commit

---
+++ aula2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe uma largura:");
+            //Console.WriteLine("Hello World!");
+            //double area = CalculaArea(35.0, 3.0);
+            CalculaArea(35, 3);
+            string largura = Console.ReadLine();
+        }
+
+         static double CalculaArea(double largura, double altura)
+        {
+            //double area = largura * altura;
+            return largura * largura;
+            
+        }
+    }
+}
