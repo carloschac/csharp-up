432cb26d489ea699e15866bbee7f3ca5dd2ce56e
CarinaRosa <carloss.chac@gmail.com>
2022-08-11 21:59:10 -0300

    up

--- aula2/Program.cs
+++ aula2/Program.cs
@@ -7,16 +7,19 @@ namespace Aula2
         static void Main(string[] args)
         {
             Console.WriteLine("Informe uma largura:");
-            //Console.WriteLine("Hello World!");
-            //double area = CalculaArea(35.0, 3.0);
-            CalculaArea(35, 3);
-            string largura = Console.ReadLine();
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe uma altura:");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double area = CalculaArea(largura,altura);
+            Console.WriteLine("A area equivale a:");
+            Console.WriteLine(area);
+            
         }
 
          static double CalculaArea(double largura, double altura)
         {
             //double area = largura * altura;
-            return largura * largura;
+            return largura * altura;
             
         }
     }
